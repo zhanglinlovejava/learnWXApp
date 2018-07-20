6d208e390091a19975dc7b091ab2a39961c8c247
zhanglin <lin.zhang01@bianlifeng.com>
2018-07-20 18:55:05 +0800

    首页添加下拉加载更多功能，完善视频播放页面

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,29 +1,39 @@
 //index.js
 //获取应用实例
 const app = getApp()
-
+var _this
+var nextPageUrl
+var firstPageUrl = 'http://baobab.kaiyanapp.com/api/v2/feed?num=1'
+var newList = new Array()
 Page({
   data: {
     list: []
   },
 
   onLoad: function() {
-    var _this = this
+    _this = this
+    _this.loadData(firstPageUrl)
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function() {
+    _this.loadData(nextPageUrl)
+  },
+  loadData: function(url) {
     wx.showLoading({
       title: '正在加载...',
     })
     wx.request({
-      url: 'http://baobab.kaiyanapp.com/api/v2/feed',
-      data: {
-        'num': '1'
-      },
+      url: url,
       header: {
         'content-type': 'application/json'
       },
       success: function(res) {
         wx.hideLoading()
-        var newList = new Array()
         var videoList = res.data.issueList[0].itemList
+        nextPageUrl = res.data.nextPageUrl
         for (var i = 0; i < videoList.length; i++) {
           if (videoList[i].type === 'video') {
             newList.push(videoList[i])
@@ -35,9 +45,7 @@ Page({
       },
       fail: function(error) {
         wx.hideLoading()
-        console.log(error)
       }
     })
-
   }
 })
--- pages/video/videoPlay.js
+++ pages/video/videoPlay.js
@@ -5,18 +5,54 @@ Page({
    * 页面的初始数据
    */
   data: {
-    videoEntity: {},
-    title:''
+    list: [],
+    url: "",
+    id: "",
+    title: "",
+    desc: "",
+    duration: "",
+    category: ""
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function(options) {
-    console.log(options.videoEntity.title)
-    // this.setData({
-    //   videoEntity: entity
-    // })
+    var _this = this
+    this.setData({
+      id: options.id,
+      title: options.title,
+      desc: options.desc,
+      duration: options.duration,
+      category: options.category,
+      url: "http://baobab.kaiyanapp.com/api/v1/playUrl?vid=" + options.id + "&resourceType=video&editionType=default&source=aliyun"
+    })
+    wx.showLoading({
+      title: '正在加载...',
+    })
+    wx.request({
+      url: 'http://baobab.kaiyanapp.com/api/v4/video/related?',
+      data: {
+        'id': _this.data.id
+      },
+      success: function (result) {
+        wx.hideLoading()
+        var oldList = result.data.itemList;
+        var newList = new Array()
+        for (var i = 0; i < oldList.length; i++) {
+          if (oldList[i].type == "videoSmallCard") {
+            newList.push(oldList[i])
+          }
+        }
+        _this.setData({
+          list: newList
+        })
+        console.log(_this.data.list)
+      },
+      fail: function (error) {
+        wx.hideLoading()
+      }
+    })
   },
 
   /**
@@ -30,7 +66,7 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function() {
-
+  
   },
 
   /**
@@ -66,5 +102,10 @@ Page({
    */
   onShareAppMessage: function() {
 
+  },
+  startPlay(url){
+    this.setData({
+      url:url
+    })
   }
 })
